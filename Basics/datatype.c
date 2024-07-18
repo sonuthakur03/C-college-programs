113a47849e3ea8fbe8608a1b7a54f58ae5455f78
Sonu Thakur <rsonuth789@gmail.com>
2024-07-18 07:48:13 +0545

    first day

---
+++ Basics/datatype.c
@@ -0,0 +1,8 @@
+// Datatype programming
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a = 03;
+    printf("%d", a);
+    return 0;
+}
---
+++ Basics/escapeSeq.c
@@ -0,0 +1,8 @@
+// Escape Sequence Programming
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    printf("Hello World \n");
+    printf("Hello World again");
+    return 0;
+}
