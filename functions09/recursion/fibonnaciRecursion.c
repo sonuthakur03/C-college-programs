404dab117fddccfadff9e4efd4e2b0cee7ccc3a9
Sonu Thakur <rsonuth789@gmail.com>
2024-09-26 19:12:46 +0545

    recursion

---
+++ functions09/recursion/fibonnaciRecursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0; // Base case
+    } else if (n == 1) {
+        return 1; // Base case
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2); 
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of terms for the Fibonacci series: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci series up to %d terms:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+
+    return 0;
+}
